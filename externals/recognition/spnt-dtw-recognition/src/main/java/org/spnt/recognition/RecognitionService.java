8eb3319d769ad3fce28cc8d0ecfe0161c123c797
mondhs <mondhs@6c5250ae-6be6-4c55-8b60-3439a918610b>
2009-05-15 18:12:07 +0000

    2776363  	 Implement recognition module

--- externals/recognition/spnt-dtw-recognition/src/main/java/org/spnt/recognition/RecognitionService.java
+++
@@ -1,8 +0,0 @@
-package org.spnt.recognition;
-
-import org.spantus.core.FrameValues;
-import org.spantus.core.FrameVectorValues;
-
-public interface RecognitionService {
-	public String match(FrameVectorValues test);
-}
