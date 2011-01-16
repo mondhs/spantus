a4fa9de3f36d3ca78fa18f76e96028358d39cd4d
mondhs <mondhs@6c5250ae-6be6-4c55-8b60-3439a918610b>
2011-01-16 11:13:17 +0000

    3101841 Multi-feature dtw pattern matching

---
+++ externals/experiment/spnt-exp-segment/src/test/java/org/spantus/exp/recognition/AbstractSegmentDirTest.java
@@ -0,0 +1,154 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.spantus.exp.recognition;
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import org.junit.Before;
+import org.spantus.core.marker.Marker;
+import org.spantus.core.marker.MarkerSet;
+import org.spantus.core.marker.MarkerSetHolder;
+import org.spantus.core.marker.MarkerSetHolder.MarkerSetHolderEnum;
+import org.spantus.externals.recognition.bean.CorpusEntry;
+import org.spantus.externals.recognition.bean.RecognitionResult;
+import org.spantus.externals.recognition.corpus.CorpusRepositoryFileImpl;
+import org.spantus.externals.recognition.services.CorpusServiceBaseImpl;
+import org.spantus.externals.recognition.services.impl.CorpusEntryExtractorFileImpl;
+import org.spantus.extractor.impl.ExtractorEnum;
+import org.spantus.logger.Logger;
+import org.spantus.segment.online.OnlineDecisionSegmentatorParam;
+
+/**
+ *
+ * @author mondhs
+ */
+public abstract class AbstractSegmentDirTest {
+
+    public final static String DIR_LEARN_WAV =
+            "/mnt/audio/MG" //            "/home/mondhs/src/garsynai/skaiciai/learn"
+            ;
+    public final static String DIR_LEARN =
+            "/mnt/audio/MG/OUTPUT/"
+//            "./target/learn-corpus/"
+            ;
+    private File learnDir;
+    private CorpusEntryExtractorFileImpl extractor;
+    private CorpusServiceBaseImpl corpusService;
+    private CorpusRepositoryFileImpl corpusRepository;
+    private static final Logger log = Logger.getLogger(AbstractSegmentDirTest.class);
+
+    @Before
+    public void onSetup() {
+        learnDir = new File(DIR_LEARN_WAV);
+        extractor = new CorpusEntryExtractorFileImpl();
+        corpusService = new CorpusServiceBaseImpl();
+        corpusRepository = new CorpusRepositoryFileImpl();
+        corpusRepository.setRepositoryPath(DIR_LEARN);
+        corpusService.setCorpus(corpusRepository);
+        extractor.setCorpusService(corpusService);
+
+        OnlineDecisionSegmentatorParam segmentionParam = new OnlineDecisionSegmentatorParam();
+        segmentionParam.setMinLength(91L);
+        segmentionParam.setMinSpace(261L);
+        segmentionParam.setExpandStart(260L);
+        segmentionParam.setExpandEnd(360L);
+
+        ExtractorEnum[] extractors = new ExtractorEnum[]{
+            ExtractorEnum.MFCC_EXTRACTOR,
+            ExtractorEnum.LPC_EXTRACTOR,
+            ExtractorEnum.FFT_EXTRACTOR,
+            ExtractorEnum.SPECTRAL_FLUX_EXTRACTOR,
+            ExtractorEnum.SIGNAL_ENTROPY_EXTRACTOR};
+
+        extractor.setSegmentionParam(segmentionParam);
+        extractor.setExtractors(extractors);
+    }
+
+    protected MarkerSet getSegementedMarkers(MarkerSetHolder markerSetHolder) {
+        MarkerSet segments = markerSetHolder.getMarkerSets().get(MarkerSetHolderEnum.word.name());
+        if (segments == null) {
+            segments = markerSetHolder.getMarkerSets().get(MarkerSetHolderEnum.phone.name());
+        }else{
+            
+            Collections2.filter(segments.getMarkers(), new Predicate<Marker>(){
+                public boolean apply(Marker filterMarker) {
+                    return "...".equals(filterMarker.getLabel().trim());
+                }
+                
+            }).clear();
+            
+        }
+        return segments;
+    }
+
+    protected void clearCorpus() {
+        for (CorpusEntry corpusEntry : corpusRepository.findAllEntries()) {
+            corpusRepository.delete(corpusEntry);
+        }
+        corpusRepository.flush();
+    }
+
+    protected void verifyMatches() {
+        File testDir = new File(learnDir, "../test");
+        for (File filePath : testDir.listFiles(new WavFileNameFilter())) {
+            List<CorpusEntry> entries = extractor.extractInMemory(filePath);
+            log.debug("accept: {0}:{1}", filePath, entries);
+            int index = 0;
+            Map<Integer, String> results = new LinkedHashMap<Integer, String>();
+            for (CorpusEntry corpusEntry : entries) {
+                RecognitionResult result = corpusService.matchByCorpusEntry(corpusEntry);
+                index++;
+                results.put(index, result.getInfo().getName());
+            }
+            for (Map.Entry<Integer, String> resultEntry : results.entrySet()) {
+                log.debug("[testExtract]result {0}:{1}", resultEntry.getKey(), resultEntry.getValue());
+            }
+        }
+    }
+
+    public CorpusRepositoryFileImpl getCorpusRepository() {
+        return corpusRepository;
+    }
+
+    public void setCorpusRepository(CorpusRepositoryFileImpl corpusRepository) {
+        this.corpusRepository = corpusRepository;
+    }
+
+    public CorpusServiceBaseImpl getCorpusService() {
+        return corpusService;
+    }
+
+    public void setCorpusService(CorpusServiceBaseImpl corpusService) {
+        this.corpusService = corpusService;
+    }
+
+    public CorpusEntryExtractorFileImpl getExtractor() {
+        return extractor;
+    }
+
+    public void setExtractor(CorpusEntryExtractorFileImpl extractor) {
+        this.extractor = extractor;
+    }
+
+    public File getLearnDir() {
+        return learnDir;
+    }
+
+    public void setLearnDir(File learnDir) {
+        this.learnDir = learnDir;
+    }
+
+    public class WavFileNameFilter implements FilenameFilter {
+
+        public boolean accept(File file, String fileName) {
+            return fileName.endsWith(".wav");
+        }
+    }
+}
---
+++ spnt-math/src/main/java/org/spantus/math/cluster/ClusterServiceJavaMLImpl.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.spantus.math.cluster;
+
+import java.util.List;
+import net.sf.javaml.clustering.Clusterer;
+import net.sf.javaml.clustering.KMeans;
+import net.sf.javaml.core.Dataset;
+import net.sf.javaml.core.DefaultDataset;
+import net.sf.javaml.core.Instance;
+import net.sf.javaml.core.SparseInstance;
+
+/**
+ *
+ * @author mondhs
+ */
+public class ClusterServiceJavaMLImpl implements ClusterService {
+
+    public ClusterCollection cluster(List<List<Float>> vectors, int clusterSize) {
+        Dataset data = new DefaultDataset();
+        for (List<Float> floats : vectors) {
+            Instance tmpInstance = new SparseInstance(floats.size());
+            int i = 0;
+            for (Float f1 : floats) {
+                tmpInstance.put(i++, f1.doubleValue());
+            }
+            data.add(tmpInstance);
+        }
+
+        Clusterer km = new KMeans(clusterSize);
+        Dataset[] clusters = km.cluster(data);
+        ClusterCollection cc = new ClusterCollection();
+        return null;
+    }
+}
