1585d8db9eec82fe4de4453e6fce1bc766d8579d
Mondhs Bus <mondhs@gmail.com>
2021-01-13 22:25:59 +0200

    deleted oudated spnt-skype module

--- externals/integration/spnt-skype/external/install.bat
+++
@@ -1,3 +0,0 @@
- mvn install:install-file -Dfile=dbus-2.6.jar -DgroupId=org.freedesktop.dbus -DartifactId=dbus -Dversion=2.6 -Dpackaging=jar -DgeneratePom=true
-
-
--- externals/integration/spnt-skype/src/main/java/org/spantus/integration/skype/handler/IncomeSkypeMessageHandler.java
+++
@@ -1,5 +0,0 @@
-package org.spantus.integration.skype.handler;
-
-public interface IncomeSkypeMessageHandler{
-    public boolean handle(String msg);
-}
